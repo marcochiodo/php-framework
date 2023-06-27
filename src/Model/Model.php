f976a38c00ab296b222462b26a8b2da6ec51c75d
Marco Chiodo <marcochio94@gmail.com>
2023-06-27 17:35:18 +0200

    bugfix

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -1,13 +1,14 @@
 <?php
+
 namespace mrblue\framework\Model;
 
 abstract class Model implements \JsonSerializable {
 
-	CONST PRIMARY_FIELD = 'id';
+	const PRIMARY_FIELD = 'id';
 
-	CONST DB_FIELDS_MAP = [];
+	const DB_FIELDS_MAP = [];
 
-	CONST IGNORE_FIELDS = [
+	const IGNORE_FIELDS = [
 		'__ignore_fields' => true,
 		'__class_properties' => true,
 		'__updated_fields' => true
@@ -20,19 +21,19 @@ abstract class Model implements \JsonSerializable {
 	private ?array $__class_properties = null;
 	protected array $__updated_fields = [];
 
-	function __construct( array $data ) {
+	function __construct(array $data) {
 		$class_properties = $this->getClassProperties();
-		foreach( $class_properties as $ReflectionProperty ){
+		foreach ($class_properties as $ReflectionProperty) {
 			$name = $ReflectionProperty->getName();
 			$possible_name = static::DB_FIELDS_MAP[$name] ?? null;
-			if( 
-				! array_key_exists($name,$data) && 
-				! ( $possible_name && array_key_exists($possible_name,$data) )
-			){
+			if (
+				!array_key_exists($name, $data) &&
+				!($possible_name && array_key_exists($possible_name, $data))
+			) {
 				$type = self::getReflectionPropertyType($ReflectionProperty);
-				if( $ReflectionProperty->hasDefaultValue() ){
+				if ($ReflectionProperty->hasDefaultValue()) {
 					$data[$name] = $ReflectionProperty->getDefaultValue();
-				} elseif( $type && is_a($type,ModelList::class,true) && ! $ReflectionProperty->getType()->allowsNull() ){
+				} elseif ($type && is_a($type, ModelList::class, true) && !$ReflectionProperty->getType()->allowsNull()) {
 					$data[$name] = [];
 				} else {
 					$data[$name] = null;
@@ -43,77 +44,79 @@ abstract class Model implements \JsonSerializable {
 		$this->import($data);
 	}
 
-	function import( array $data ) {
+	function import(array $data) {
 		$class_properties = $this->getClassProperties();
-		foreach( $data as $key => $value ){
-			$key = array_search($key,static::DB_FIELDS_MAP,true) ? : $key;
+		foreach ($data as $key => $value) {
+			$key = array_search($key, static::DB_FIELDS_MAP, true) ?: $key;
 			$ReflectionProperty = $class_properties[$key] ?? null;
-			if( ! $ReflectionProperty ){
+			if (!$ReflectionProperty) {
 				continue;
 			}
-			if( $value !== null ){
+			if ($value !== null) {
 
 				$ReflectionType = $ReflectionProperty->getType();
 
-				if( $ReflectionType instanceof \ReflectionNamedType && ! $ReflectionType->isBuiltin() ){
+				if ($ReflectionType instanceof \ReflectionNamedType && !$ReflectionType->isBuiltin()) {
 					$type = $ReflectionType->getName();
 
-					if( ! is_a($value,$type) ){
-						if( is_a($type,self::class,true) ){
+					if (!is_a($value, $type)) {
+						if (is_a($type, self::class, true)) {
 							$class_target = $type::getFinalClass($value);
 							$value = new $class_target($value);
 						} else {
-							$value = $this->valueFrom($value,$type);
+							$value = $this->valueFrom($value, $type);
 						}
 					}
-					
 				}
 			}
 
 
-			$this->setProp($key , $value);
+			$this->setProp($key, $value);
 		}
 	}
 
-	protected function setProp( string $key , mixed $value ) {
-        $this->{$key} = $value;
+	protected function setProp(string $key, mixed $value) {
+		$this->{$key} = $value;
 	}
 
-	function export() :array {
+	function export(): array {
 		$export = [];
-		foreach( $this->getClassProperties() as $ReflectionProperty ){
+		foreach ($this->getClassProperties() as $ReflectionProperty) {
 			$key = $ReflectionProperty->getName();
 			$value = $this->{$key};
-			if( $value instanceof Model || $value instanceof ModelList ){
+			if ($value instanceof Model || $value instanceof ModelList) {
 				$value = $value->export();
 			}
-			
+
 			$export[$key] = $value;
 		}
 		return $export;
 	}
 
-	function exportToDb( bool $updated_only = false ) :array {
+	function exportToDb(bool $updated_only = false): array {
 		$export = [];
-		foreach( $this->getClassProperties() as $ReflectionProperty ){
+		foreach ($this->getClassProperties() as $ReflectionProperty) {
 			$key = $ReflectionProperty->getName();
 			$prop_type = self::getReflectionPropertyType($ReflectionProperty);
 			$value = $this->{$key};
 
-			if( $value instanceof ModelList || ($prop_type && is_a($prop_type,ModelList::class,true)) ){
+			if ($value instanceof ModelList || ($prop_type && is_a($prop_type, ModelList::class, true))) {
 				continue;
 			}
 
-			if( $updated_only ){
+			if ($updated_only) {
 
-				if( in_array($key,$this->__updated_fields) ){
-					if( $value instanceof Model ){
+				if (in_array($key, $this->__updated_fields)) {
+					if ($value instanceof Model) {
 						$value = $value->exportToDb();
+						if (!$value) {
+							$value = new \stdClass;
+						}
 					}
 				} else {
-					if( $value instanceof Model ){
+					if ($value instanceof Model) {
 						$value = $value->exportToDb(true);
-						if( ! $value ){
+						if (!$value) {
 							continue;
 						}
 					} else {
@@ -121,12 +124,15 @@ abstract class Model implements \JsonSerializable {
 					}
 				}
 			} else {
-				if( $value instanceof Model ){
+				if ($value instanceof Model) {
 					$value = $value->exportToDb();
+					if (!$value) {
+						$value = new \stdClass;
+					}
 				}
 			}
 
-			if( isset(static::DB_FIELDS_MAP[$key]) ){
+			if (isset(static::DB_FIELDS_MAP[$key])) {
 				$key = static::DB_FIELDS_MAP[$key];
 			}
 
@@ -137,10 +143,10 @@ abstract class Model implements \JsonSerializable {
 
 	function jsonSerialize(): mixed {
 		$export = [];
-		foreach( $this->getClassProperties() as $ReflectionProperty ){
+		foreach ($this->getClassProperties() as $ReflectionProperty) {
 			$key = $ReflectionProperty->getName();
 			$value = $this->{$key};
-			if( $value instanceof Model || $value instanceof ModelList || $value instanceof \JsonSerializable ){
+			if ($value instanceof Model || $value instanceof ModelList || $value instanceof \JsonSerializable) {
 				$export[$key] = $value->jsonSerialize();
 			} else {
 				$export[$key] = $this->valueToJson($value);
@@ -149,29 +155,30 @@ abstract class Model implements \JsonSerializable {
 		return $export;
 	}
 
-	function update( array $data ) :self {
+	function update(array $data): self {
 		$this->import($data);
 		$this->updateSignal(array_keys($data));
 		return $this;
 	}
 
-	function updateSignal( array $fields ) {
+	function updateSignal(array $fields) {
 		$this->__updated_fields = array_unique(array_merge(
-			$this->__updated_fields,self::filterDataValues($fields,array_keys($this->getClassProperties()))
+			$this->__updated_fields,
+			self::filterDataValues($fields, array_keys($this->getClassProperties()))
 		));
 	}
 
 	/**
 	 * @return \ReflectionProperty[] 
 	 */
-	function getClassProperties() :array {
-		if( $this->__class_properties === null ){
+	function getClassProperties(): array {
+		if ($this->__class_properties === null) {
 			$ReflectionClass = new \ReflectionClass($this);
 			$class_properties = $ReflectionClass->getProperties();
 			$this->__class_properties = [];
-			foreach( $class_properties as $ReflectionProperty ){
+			foreach ($class_properties as $ReflectionProperty) {
 				$prop_name = $ReflectionProperty->getName();
-				if( ! isset($this->__ignore_fields[$prop_name]) ){
+				if (!isset($this->__ignore_fields[$prop_name])) {
 					$this->__class_properties[$ReflectionProperty->getName()] = $ReflectionProperty;
 				}
 			}
@@ -180,69 +187,68 @@ abstract class Model implements \JsonSerializable {
 		return $this->__class_properties;
 	}
 
-	function valueFrom( $value , string $type ){
+	function valueFrom($value, string $type) {
 
-		if( is_a($type,\BackedEnum::class,true) ){
+		if (is_a($type, \BackedEnum::class, true)) {
 			return $type::from($value);
-		} elseif( is_a($type,\DateTimeInterface::class,true) ){
+		} elseif (is_a($type, \DateTimeInterface::class, true)) {
 			$class_target = $type === \DateTimeInterface::class ? \DateTimeImmutable::class : $type;
-			if( $value instanceof \DateTimeInterface ){
+			if ($value instanceof \DateTimeInterface) {
 				return $class_target::createFromInterface($value);
 			} else {
 				return new $class_target($value);
 			}
-		} elseif( class_exists($type) ){
+		} elseif (class_exists($type)) {
 			return new $type($value);
 		} else {
 			return $value;
 		}
 	}
 
-	function valueToDb( $value ) {
-		if( $value instanceof \BackedEnum ){
+	function valueToDb($value) {
+		if ($value instanceof \BackedEnum) {
 			return $value->value;
-		} elseif( $value instanceof \DateTimeZone ){
+		} elseif ($value instanceof \DateTimeZone) {
 			return $value->getName();
 		} else {
 			return $value;
 		}
 	}
 
-	function valueToJson( $value ) {
-		if( $value instanceof \BackedEnum ){
+	function valueToJson($value) {
+		if ($value instanceof \BackedEnum) {
 			return $value->value;
-		} elseif( $value instanceof \DateTimeInterface ){
-			 return $value->format(\DateTimeInterface::ATOM);
-		} elseif( $value instanceof \DateTimeZone ){
+		} elseif ($value instanceof \DateTimeInterface) {
+			return $value->format(\DateTimeInterface::ATOM);
+		} elseif ($value instanceof \DateTimeZone) {
 			return $value->getName();
 		} else {
 			return $value;
 		}
 	}
 
-	static function getFinalClass( array $data ) :string {
+	static function getFinalClass(array $data): string {
 		return static::class;
 	}
 
-	static function filterDataKeys( array $data , array $allowed ) :array {
+	static function filterDataKeys(array $data, array $allowed): array {
 
-		return array_filter($data,fn($key) => in_array($key,$allowed),ARRAY_FILTER_USE_KEY);
+		return array_filter($data, fn ($key) => in_array($key, $allowed), ARRAY_FILTER_USE_KEY);
 	}
 
-	static function filterDataValues( array $data , array $allowed ) :array {
+	static function filterDataValues(array $data, array $allowed): array {
 
-		return array_filter($data,fn($value) => in_array($value,$allowed));
+		return array_filter($data, fn ($value) => in_array($value, $allowed));
 	}
 
-	static function getReflectionPropertyType( \ReflectionProperty $ReflectionProperty ) :string {
+	static function getReflectionPropertyType(\ReflectionProperty $ReflectionProperty): string {
 
 		$ReflectionType = $ReflectionProperty->getType();
 
-		if( $ReflectionType instanceof \ReflectionNamedType ){
+		if ($ReflectionType instanceof \ReflectionNamedType) {
 			return $ReflectionType->getName();
 		} else {
 			return null;
 		}
 	}
 }
-
