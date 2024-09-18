142d7aeb545ce3ef46509bfcf6ef40068205e222
Marco Chiodo <marcochio94@gmail.com>
2024-09-18 16:03:58 +0200

    Improved request

--- src/Utils/Request.php
+++ src/Utils/Request.php
@@ -16,6 +16,18 @@ class Request {
     public readonly ?string $referer;
     public readonly ?string $host;
 
+    public readonly ?string $request_uri;
+
+    public readonly ?string $request_scheme;
+    public readonly ?string $request_host;
+    public readonly ?int $request_port;
+    public readonly ?string $request_user;
+    public readonly ?string $request_pass;
+    public readonly ?string $request_path;
+    public readonly ?string $request_query;
+    public readonly ?string $request_fragment;
+
+
     function __construct() {
         if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
             $this->ip = trim(explode(',', $_SERVER['HTTP_X_FORWARDED_FOR'])[0]);
@@ -37,7 +49,33 @@ class Request {
         $this->referer = $_SERVER['HTTP_REFERER'] ?? null;
         $this->host = $_SERVER['HTTP_HOST'] ?? null;
 
-        $this->server_name = $_SERVER['SERVER_NAME'] ?? $_SERVER['HTTP_HOST'] ?? $_SERVER['SERVER_ADDR'] ?? null;
+        if (! empty($_SERVER['SERVER_NAME'])) {
+            $this->server_name = $_SERVER['SERVER_NAME'];
+        } elseif ($this->host) {
+            $this->server_name = $this->host;
+        } elseif (! empty($_SERVER['SERVER_ADDR'])) {
+            $this->server_name = $_SERVER['SERVER_ADDR'];
+        } else {
+            $this->server_name = null;
+        }
+
+        $this->request_uri = $_SERVER['REQUEST_URI'] ?? null;
+        $request_uri_parts = $this->request_uri ? parse_url($this->request_uri) : [];
+
+        foreach (
+            [
+                'scheme',
+                'host',
+                'port',
+                'user',
+                'pass',
+                'path',
+                'query',
+                'fragment'
+            ] as $part
+        ) {
+            $this->{'server_' . $part} = $request_uri_parts[$part] ?? null;
+        }
     }
 
     static function getGlobalInstance(): self {
